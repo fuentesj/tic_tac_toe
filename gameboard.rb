bb4d2ff9b396f2c24a520c483d3ff50a1f04d7e4
fuentesj <jonathan.wilfredo.fuentes@gmail.com>
2017-10-06 06:48:56 -0700

    Fixes formatting issues when printing game board to terminal.

--- gameboard.rb
+++ gameboard.rb
@@ -11,23 +11,53 @@ class GameBoard
 	def draw_game_board
 		@row_array.each_with_index do |current_row, row_index|
 			current_row.each_with_index do |current_element, index|
-				if (index == current_row.length - 1)
-					print current_element + "\n"
+				unless current_element.empty?
+					if (index == current_row.length - 1)
+						printf "\s#{current_element}\s\n"
+					else
+						print "\s#{current_element}\s|"
+					end
 				else
-					print current_element + "  | "
+					if (index == current_row.length - 1)
+						print "\s\s\s\n"
+					else
+						print "\s\s\s|"
+					end
 				end
 			end 
 			if (row_index != current_row.length-1)
-				print "----------\n"
+				print "-----------\n"
 			end
 		end
 	end
 
-	def add_new_square(first_index, second_index)
-		if @row_array[first_index][second_index].empty?
-			@row_array[first_index][second_index] = "X"
+	def check_if_game_over
+
+	end
+
+	def add_new_square(first_index, second_index, player)
+		if player == "player1"
+			player_char = "X"
 		else
-			puts "Square already taken. Please choose another square."
+			player_char = "O"
+		end
+		if @row_array[first_index][second_index].empty?
+			@row_array[first_index][second_index] = player_char
+			true
+		else
+			false
+		end
+	end
+
+	def computer_move()
+		sleep(3)
+		row_index = 0 + rand(2)
+		col_index = 0 + rand(2)
+		computer_added_square = add_new_square row_index, col_index, "player2"
+		while (computer_added_square == false)
+			row_index = 0 + rand(2)
+			col_index = 0 + rand(2)
+			computer_added_square = add_new_square row_index, col_index, "player2"
 		end
 	end
 
--- main.rb
+++ main.rb
@@ -9,30 +9,40 @@ puts "ml | mc | mr"
 puts "------------"
 puts "bl | bc | br"
 puts "---------------------"
-puts "Enter new square: "
+puts "Player's move: "
 
 while game_board.is_game_over? == false
 	next_square = gets.chomp
 	if (next_square == "tl")
-		game_board.add_new_square 0, 0
+		game_board.add_new_square 0, 0, "player1"
 	elsif (next_square == "tc")
-		game_board.add_new_square 0, 1
+		game_board.add_new_square 0, 1, "player1"
 	elsif (next_square == "tr")
-		game_board.add_new_square 0, 2
+		game_board.add_new_square 0, 2, "player1"
 	elsif (next_square == "ml")
-		game_board.add_new_square 1, 0
+		game_board.add_new_square 1, 0, "player1"
 	elsif (next_square == "mc")
-		game_board.add_new_square 1, 1
+		game_board.add_new_square 1, 1, "player1"
 	elsif (next_square == "mr")
-		game_board.add_new_square 1, 2
+		game_board.add_new_square 1, 2, "player1"
 	elsif (next_square == "bl")	
-		game_board.add_new_square 2, 0
+		game_board.add_new_square 2, 0, "player1"
 	elsif (next_square == "bc")
-		game_board.add_new_square 2, 1
+		game_board.add_new_square 2, 1, "player1"
 	elsif (next_square == "br")
-		game_board.add_new_square 2, 2
+		game_board.add_new_square 2, 2, "player1"
 	else
 		puts "Invalid square entered."
 	end
-		game_board.draw_game_board
+	game_board.check_if_game_over
+	game_board.draw_game_board
+	puts ""
+	puts "---------------------"
+	puts "Computer's move:"
+	game_board.computer_move
+	game_board.check_if_game_over
+	game_board.draw_game_board
+	puts ""
+	puts "---------------------"
+	puts "Player move: "
 end
